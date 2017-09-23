efb97a20b598d1e5abc165ffc6630a76aa3774b9
natansevero <natanseverolp@gmail.com>
2017-09-22 21:53:55 -0300

    Trabalhando com componentes filhos

---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='Natan' lastName='Silva' />
+        <Member name='Jonas' lastName='Silva' />
+        <Member name='Diones' lastName='Silva' />
+    </div>
+)
