c2644fbc4b8656cafd03e65101ddc3327c055317
natansevero <natanseverolp@gmail.com>
2017-09-22 22:28:25 -0300

    Adiconando configuração do babel-plugin-transform-object-rest-spread

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-object-rest-spread']
                 }
             }
         ]
